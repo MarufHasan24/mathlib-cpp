6aaf17e2381718e2ce4d5c5576f2334609fb9685
dimebot <sp14apr@gmail.com>
2023-10-04 20:46:32 +0530

    Added length conversions

---
+++ lengthconversion.cpp
@@ -0,0 +1,187 @@
+#include <bits/stdc++.h>
+namespace mlb{
+	// Function to convert meters to kilometers
+    double metersToKilometers(double meters) {
+        return meters / 1000.0; 
+    }
+
+    // Function to convert meters to centimeters
+    double metersToCentimeters(double meters) {
+        return meters * 100.0; 
+    }
+
+    // Function to convert meters to millimeters
+    double metersToMillimeters(double meters) {
+        return meters * 1000.0; 
+    }
+
+    // Function to convert kilometers to meters
+    double kilometersToMeters(double kilometers) {
+        return kilometers * 1000.0; 
+    }
+
+    // Function to convert centimeters to meters
+    double centimetersToMeters(double centimeters) {
+        return centimeters / 100.0; 
+    }
+
+    // Function to convert millimeters to meters
+    double millimetersToMeters(double millimeters) {
+        return millimeters / 1000.0; 
+    }
+    
+        // Function to convert inches to feet
+    double inchesToFeet(double inches) {
+        return inches / 12.0; 
+    }
+
+    // Function to convert inches to yards
+    double inchesToYards(double inches) {
+        return inches / 36.0; 
+    }
+
+    // Function to convert inches to miles
+    double inchesToMiles(double inches) {
+        return inches / 63360.0; 
+    }
+
+    // Function to convert feet to yards
+    double feetToYards(double feet) {
+        return feet / 3.0; 
+    }
+
+    // Function to convert feet to miles
+    double feetToMiles(double feet) {
+        return feet / 5280.0; 
+    }
+
+    // Function to convert yards to inches
+    double yardsToInches(double yards) {
+        return yards * 36.0; 
+    }
+
+    // Function to convert yards to miles
+    double yardsToMiles(double yards) {
+        return yards / 1760.0; 
+    }
+    
+    // Function to convert inches to centimeters
+    double inchesToCentimeters(double inches) {
+        return inches * 2.54; 
+    }
+
+    // Function to convert inches to millimeters
+    double inchesToMillimeters(double inches) {
+        return inches * 25.4; 
+    }
+
+    // Function to convert inches to kilometers
+    double inchesToKilometers(double inches) {
+        return inches * 0.0000254; 
+    }
+
+    // Function to convert centimeters to inches
+    double centimetersToInches(double centimeters) {
+        return centimeters * 0.393701; 
+    }
+
+    // Function to convert millimeters to inches
+    double millimetersToInches(double millimeters) {
+        return millimeters * 0.0393701; 
+    }
+
+    // Function to convert kilometers to inches
+    double kilometersToInches(double kilometers) {
+        return kilometers * 39370.1; 
+    }    
+
+    // Function to convert feet to centimeters
+    double feetToCentimeters(double feet) {
+        return feet * 30.48; 
+    }
+
+    // Function to convert feet to millimeters
+    double feetToMillimeters(double feet) {
+        return feet * 304.8; 
+    }
+
+    // Function to convert feet to kilometers
+    double feetToKilometers(double feet) {
+        return feet * 0.0003048; 
+    }
+
+    // Function to convert centimeters to feet
+    double centimetersToFeet(double centimeters) {
+        return centimeters / 30.48; 
+    }
+
+    // Function to convert millimeters to feet
+    double millimetersToFeet(double millimeters) {
+        return millimeters / 304.8; 
+    }
+
+    // Function to convert kilometers to feet
+    double kilometersToFeet(double kilometers) {
+        return kilometers / 0.0003048; 
+    }
+    
+    // Function to convert yards to centimeters
+    double yardsToCentimeters(double yards) {
+        return yards * 91.44; 
+    }
+
+    // Function to convert yards to millimeters
+    double yardsToMillimeters(double yards) {
+        return yards * 914.4; 
+    }
+
+    // Function to convert yards to kilometers
+    double yardsToKilometers(double yards) {
+        return yards * 0.0009144; 
+    }
+
+    // Function to convert centimeters to yards
+    double centimetersToYards(double centimeters) {
+        return centimeters / 91.44; 
+    }
+
+    // Function to convert millimeters to yards
+    double millimetersToYards(double millimeters) {
+        return millimeters / 914.4; 
+    }
+
+    // Function to convert kilometers to yards
+    double kilometersToYards(double kilometers) {
+        return kilometers / 0.0009144; 
+    }
+
+    // Function to convert miles to centimeters
+    double milesToCentimeters(double miles) {
+        return miles * 160934.0; 
+    }
+
+    // Function to convert miles to millimeters
+    double milesToMillimeters(double miles) {
+        return miles * 1609340.0; 
+    }
+
+    // Function to convert miles to kilometers
+    double milesToKilometers(double miles) {
+        return miles * 1.60934; 
+    }
+
+    // Function to convert centimeters to miles
+    double centimetersToMiles(double centimeters) {
+        return centimeters / 160934.0; 
+    }
+
+    // Function to convert millimeters to miles
+    double millimetersToMiles(double millimeters) {
+        return millimeters / 1609340.0; 
+    }
+
+    // Function to convert kilometers to miles
+    double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934; 
+    }
+}
