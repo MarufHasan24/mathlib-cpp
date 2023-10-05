94243ec31dcb2018a99d6773e807020848cce626
Marufhasan24 <gamerid703@gmail.com>
2023-10-05 11:29:31 +0600

    fixed

--- scripts/built.sh
+++ scripts/built.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-for entry in `ls ./../`; do
+path="https://raw.githubusercontent.com/MarufHasan24/mathlib-cpp/master/"
+for entry in `ls ${path}/`; do
 if [[ $entry == *".cpp" ]]; then
-    if [ -f ./../test/${entry} ]; then
-        g++ ./../test/$entry -std=c++11 -o ./../test/out/${entry%.*}.out
-        echo "Built ${entry%.*}.out"
+    if [ -f ${path}/test/${entry} ]; then
+        g++ ${path}/test/$entry -std=c++11 -o ${path}/test/out/${entry%.*}.out
+        echo "Successfullly built ${entry%.*}.out"
     else
         echo "No test file for ${entry%.*}.cpp"
         exit 1
