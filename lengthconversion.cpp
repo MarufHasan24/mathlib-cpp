aba0228094157d1c8861720596a984b6ae8e039a
dimebot <sp14apr@gmail.com>
2023-10-05 15:22:53 +0530

    Removed intermetric conversions and added error exception <0 values.

--- lengthconversion.cpp
+++ lengthconversion.cpp
@@ -1,187 +1,221 @@
 #include <bits/stdc++.h>
-namespace mlb{
-	// Function to convert meters to kilometers
-    double metersToKilometers(double meters) {
-        return meters / 1000.0; 
-    }
 
-    // Function to convert meters to centimeters
-    double metersToCentimeters(double meters) {
-        return meters * 100.0; 
-    }
-
-    // Function to convert meters to millimeters
-    double metersToMillimeters(double meters) {
-        return meters * 1000.0; 
-    }
-
-    // Function to convert kilometers to meters
-    double kilometersToMeters(double kilometers) {
-        return kilometers * 1000.0; 
-    }
-
-    // Function to convert centimeters to meters
-    double centimetersToMeters(double centimeters) {
-        return centimeters / 100.0; 
-    }
-
-    // Function to convert millimeters to meters
-    double millimetersToMeters(double millimeters) {
-        return millimeters / 1000.0; 
-    }
-    
-        // Function to convert inches to feet
-    double inchesToFeet(double inches) {
+namespace mlb {
+    double inch2ft(double inches) {
+        if (inches < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return inches / 12.0; 
     }
 
-    // Function to convert inches to yards
-    double inchesToYards(double inches) {
+    double inch2yard(double inches) {
+        if (inches < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return inches / 36.0; 
     }
 
-    // Function to convert inches to miles
-    double inchesToMiles(double inches) {
+    double inch2mile(double inches) {
+        if (inches < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return inches / 63360.0; 
     }
 
-    // Function to convert feet to yards
-    double feetToYards(double feet) {
+    double ft2yard(double feet) {
+        if (feet < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return feet / 3.0; 
     }
 
-    // Function to convert feet to miles
-    double feetToMiles(double feet) {
+    double ft2mile(double feet) {
+        if (feet < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return feet / 5280.0; 
     }
 
-    // Function to convert yards to inches
-    double yardsToInches(double yards) {
+    double yard2inch(double yards) {
+        if (yards < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return yards * 36.0; 
     }
 
-    // Function to convert yards to miles
-    double yardsToMiles(double yards) {
+    double yard2mile(double yards) {
+        if (yards < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return yards / 1760.0; 
     }
-    
-    // Function to convert inches to centimeters
-    double inchesToCentimeters(double inches) {
+
+    double inch2cm(double inches) {
+        if (inches < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return inches * 2.54; 
     }
 
-    // Function to convert inches to millimeters
-    double inchesToMillimeters(double inches) {
+    double inch2mm(double inches) {
+        if (inches < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return inches * 25.4; 
     }
 
-    // Function to convert inches to kilometers
-    double inchesToKilometers(double inches) {
+    double inch2km(double inches) {
+        if (inches < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return inches * 0.0000254; 
     }
 
-    // Function to convert centimeters to inches
-    double centimetersToInches(double centimeters) {
+    double cm2inch(double centimeters) {
+        if (centimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return centimeters * 0.393701; 
     }
 
-    // Function to convert millimeters to inches
-    double millimetersToInches(double millimeters) {
+    double mm2inch(double millimeters) {
+        if (millimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return millimeters * 0.0393701; 
     }
 
-    // Function to convert kilometers to inches
-    double kilometersToInches(double kilometers) {
+    double km2inch(double kilometers) {
+        if (kilometers < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return kilometers * 39370.1; 
     }    
 
-    // Function to convert feet to centimeters
-    double feetToCentimeters(double feet) {
+    double ft2cm(double feet) {
+        if (feet < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return feet * 30.48; 
     }
 
-    // Function to convert feet to millimeters
-    double feetToMillimeters(double feet) {
+    double ft2mm(double feet) {
+        if (feet < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return feet * 304.8; 
     }
 
-    // Function to convert feet to kilometers
-    double feetToKilometers(double feet) {
+    double ft2km(double feet) {
+        if (feet < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return feet * 0.0003048; 
     }
 
-    // Function to convert centimeters to feet
-    double centimetersToFeet(double centimeters) {
+    double cm2ft(double centimeters) {
+        if (centimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return centimeters / 30.48; 
     }
 
-    // Function to convert millimeters to feet
-    double millimetersToFeet(double millimeters) {
+    double mm2ft(double millimeters) {
+        if (millimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return millimeters / 304.8; 
     }
 
-    // Function to convert kilometers to feet
-    double kilometersToFeet(double kilometers) {
+    double km2ft(double kilometers) {
+        if (kilometers < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return kilometers / 0.0003048; 
     }
-    
-    // Function to convert yards to centimeters
-    double yardsToCentimeters(double yards) {
+
+    double yard2cm(double yards) {
+        if (yards < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return yards * 91.44; 
     }
 
-    // Function to convert yards to millimeters
-    double yardsToMillimeters(double yards) {
+    double yard2mm(double yards) {
+        if (yards < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return yards * 914.4; 
     }
 
-    // Function to convert yards to kilometers
-    double yardsToKilometers(double yards) {
+    double yard2km(double yards) {
+        if (yards < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return yards * 0.0009144; 
     }
 
-    // Function to convert centimeters to yards
-    double centimetersToYards(double centimeters) {
+    double cm2yard(double centimeters) {
+        if (centimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return centimeters / 91.44; 
     }
 
-    // Function to convert millimeters to yards
-    double millimetersToYards(double millimeters) {
+    double mm2yard(double millimeters) {
+        if (millimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return millimeters / 914.4; 
     }
 
-    // Function to convert kilometers to yards
-    double kilometersToYards(double kilometers) {
+    double km2yard(double kilometers) {
+        if (kilometers < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return kilometers / 0.0009144; 
     }
 
-    // Function to convert miles to centimeters
-    double milesToCentimeters(double miles) {
+    double mile2cm(double miles) {
+        if (miles < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return miles * 160934.0; 
     }
 
-    // Function to convert miles to millimeters
-    double milesToMillimeters(double miles) {
+    double mile2mm(double miles) {
+        if (miles < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return miles * 1609340.0; 
     }
 
-    // Function to convert miles to kilometers
-    double milesToKilometers(double miles) {
+    double mile2km(double miles) {
+        if (miles < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return miles * 1.60934; 
     }
 
-    // Function to convert centimeters to miles
-    double centimetersToMiles(double centimeters) {
+    double cm2mile(double centimeters) {
+        if (centimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return centimeters / 160934.0; 
     }
 
-    // Function to convert millimeters to miles
-    double millimetersToMiles(double millimeters) {
+    double mm2mile(double millimeters) {
+        if (millimeters < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return millimeters / 1609340.0; 
     }
 
-    // Function to convert kilometers to miles
-    double kilometersToMiles(double kilometers) {
+    double km2mile(double kilometers) {
+        if (kilometers < 0) {
+            throw std::invalid_argument("Input value must be non-negative");
+        }
         return kilometers / 1.60934; 
     }
 }
+
