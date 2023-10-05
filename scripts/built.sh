3c9629eaa0477e8a0c75f5fc2725f0d05cf52000
Marufhasan24 <gamerid703@gmail.com>
2023-10-05 16:45:08 +0600

    dvb

--- scripts/built.sh
+++ scripts/built.sh
@@ -6,7 +6,7 @@ for entry in `ls ${path}/`; do
 if [[ $entry == *".cpp" ]]; then
     if [ -f ${path}/test/${entry} ]; then
         g++ ${path}/test/$entry -std=c++11 -o ${path}/test/out/${entry%.*}.out
-        echo "Successfullly built ${entry%.*}.out"
+        echo "Successfully built ${entry%.*}.out"
     else
         echo "No test file for ${entry%.*}.cpp"
         exit 1
