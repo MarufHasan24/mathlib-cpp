2d9f7504a2a055944060aec00eab2bbb45593686
Marufhasan24 <gamerid703@gmail.com>
2023-10-05 19:19:41 +0600

    fixed

--- scripts/out.sh
+++ scripts/out.sh
@@ -1,15 +1,8 @@
-#!/usr/bin
-
-path=$(dirname $0)/..
-echo "Building all files in ${path}/test"
-for entry in `ls ${path}/`; do
-if [[ $entry == *".cpp" ]]; then
-    if [ -f ${path}/test/${entry} ]; then
-        g++ ${path}/test/$entry -std=c++11 -o ${path}/test/out/${entry%.*}.out
-        echo "Successfully built ${entry%.*}.out"
-    else
-        echo "No test file for ${entry%.*}.cpp"
-        exit 1
-    fi
-fi
+path=$(pwd)/../test
+cd $path
+for entry in $(ls); do
+ if [[ $entry == *".cpp" ]]; then
+  echo "Compiling $entry into ${entry%.*}.out"
+     g++ $entry -o $path/out/${entry%.*}.out -Wfatal-errors -std=c++11
+ fi
 done
